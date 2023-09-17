bf16d33f8bf01191b55e84ee7e0337f87d988272
Gheorghii28 <popovici.gheorghii89@gmail.com>
2023-09-17 17:26:00 +0200

    HTML and SCSS improvements in forgotpassword-component

--- src/app/forgotpassword/forgotpassword.component.ts
+++ src/app/forgotpassword/forgotpassword.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgotpassword',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class ForgotpasswordComponent {
 
+  forgotFormular: FormGroup = new FormGroup({});
+  emailField: any;
+  emailWarningText: string = '';
+  isEmailInvalid: boolean = false;
+
+  @ViewChild('emailField', { static: true }) emailFieldRef!: ElementRef<HTMLElement>;
+  @ViewChild('forgotContent', { static: true }) forgotSectionRef!: ElementRef<HTMLElement>;
+
+  constructor(private formBuilder: FormBuilder) {
+    
+  }
+
+  ngOnInit() {
+    this.forgotFormular = this.formBuilder.group({
+      emailField: ['', [Validators.required, Validators.email]],
+      passwordField: ['', Validators.required]
+    });
+  }
+
+  ngAfterViewInit() {
+    this.initializeFields();
+  }
+
+  initializeFields() {
+    this.emailField = this.emailFieldRef.nativeElement;
+  }
+
+  reloadPage() {
+    window.location.reload();
+  }
+
+  onSubmit() {
+
+  }
 }
