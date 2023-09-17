3e4712e90ba4515ac0b67e05cb355dd2a4260cff
Gheorghii28 <popovici.gheorghii89@gmail.com>
2023-09-17 12:55:20 +0200

    Optimized login functionality

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,14 @@
-import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild, ChangeDetectorRef, ChangeDetectionStrategy } from '@angular/core';
 import { Router } from '@angular/router';
 import { ValidationService } from '../services/validation/validation.service';
 import { UsersListServices } from '../services/firebase-services/users-list.services';
-import { Validators, FormBuilder, FormGroup, } from '@angular/forms'
+import { Validators, FormBuilder, FormGroup } from '@angular/forms'
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LoginComponent implements OnInit {
 
@@ -19,6 +20,11 @@ export class LoginComponent implements OnInit {
   emailWarningText: string = '';
   passwordWarningText: string = '';
   loginWarningText: string = '';
+  isEmailInvalid: boolean = false;
+  isPasswordInvalid: boolean = false;
+  isLoginInvalid: boolean = false;
+  rememberMeKey: string = 'rememberMe';
+  rememberMe: boolean = false;
 
   @ViewChild('loginContent', { static: true }) loginSectionRef!: ElementRef<HTMLElement>;
   @ViewChild('emailField', { static: true }) emailFieldRef!: ElementRef<HTMLElement>;
@@ -30,19 +36,22 @@ export class LoginComponent implements OnInit {
     private validation: ValidationService,
     private usersListRef: UsersListServices,
     private router: Router,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private cdRef: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
     this.loginFormular = this.formBuilder.group({
       emailField: ['', [Validators.required, Validators.email]],
-      passwordField: ['', Validators.required],
+      passwordField: ['', Validators.required]
     });
   }
 
   ngAfterViewInit() {
+    this.cdRef.detectChanges();
     this.removeSingleAnimationClass();
     this.initializeFields();
+    this.autoLogin();
   }
 
   initializeFields() {
@@ -63,13 +72,14 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
+    this.setRememberMe(this.rememberMe)
     this.router.navigate(['/summary']);
   }
 
-  isUserValid(email:string, password:string) {
+  isUserValid(email: string, password: string) {
     let isValid = false;
-    this.usersListRef['userList'].forEach((user:any) => {
-      if(email === user['email'] &&  password === user['password']) {
+    this.usersListRef['userList'].forEach((user: any) => {
+      if (email === user['email'] && password === user['password']) {
         this.userId = user['userId'];
         isValid = true;
       }
@@ -77,37 +87,44 @@ export class LoginComponent implements OnInit {
     return isValid;
   }
 
-  isFieldEmpty(field:string, value:string) {
+  isFieldEmpty(field: string, value: string) {
     return this.loginFormular.controls[field].errors && value.length == 0;
   }
 
-  isInvalidEmail(emailValue:string) {
+  isInvalidEmail(emailValue: string) {
     return !this.validation.isEmailValid(emailValue) && emailValue.length > 0;
   }
 
-  isInvalidPassword(passwordValue:string) {
+  isInvalidPassword(passwordValue: string) {
     return !this.validation.isPasswordValid(passwordValue) && passwordValue.length > 0;
   }
 
-  isFieldsValid(emailValue:string, passwordValue:string) {
+  isFieldsValid(emailValue: string, passwordValue: string) {
     let isValid = true;
+    this.isEmailInvalid = false;
+    this.isPasswordInvalid = false;
     this.emailWarningText = '';
     this.passwordWarningText = '';
+
     if (this.isFieldEmpty('emailField', emailValue)) {
       this.emailWarningText = 'This field is required';
       isValid = false;
+      this.isEmailInvalid = !isValid;
     }
     if (this.isInvalidEmail(emailValue)) {
       this.emailWarningText = 'The entered email address is invalid.';
       isValid = false;
+      this.isEmailInvalid = !isValid;
     }
     if (this.isFieldEmpty('passwordField', passwordValue)) {
       this.passwordWarningText = 'This field is required';
       isValid = false;
+      this.isPasswordInvalid = !isValid;
     }
     if (this.isInvalidPassword(passwordValue)) {
       this.passwordWarningText = 'The entered password is invalid.'
       isValid = false;
+      this.isPasswordInvalid = !isValid;
     }
     return isValid;
   }
@@ -115,17 +132,19 @@ export class LoginComponent implements OnInit {
   loginFormIsInvalid(emailValue: string, passwordValue: string): boolean {
     return this.loginFormular.invalid || !this.isUserValid(emailValue, passwordValue);
   }
-  
 
-  checkFields(emailValue:string, passwordValue:string) {
-    if(this.isFieldsValid(emailValue, passwordValue)) {
+
+  checkFields(emailValue: string, passwordValue: string) {
+    this.isLoginInvalid = false;
+    if (this.isFieldsValid(emailValue, passwordValue)) {
       this.loginWarningText = 'Login unsuccessful. Please verify your login information.';
+      this.isLoginInvalid = true;
     }
   }
 
   onSubmit() {
-    let emailValue = this.loginFormular.controls['emailField'].value
-    let passwordValue = this.loginFormular.controls['passwordField'].value
+    let emailValue = this.loginFormular.controls['emailField'].value;
+    let passwordValue = this.loginFormular.controls['passwordField'].value;
     this.loginWarningText = '';
     if (this.loginFormIsInvalid(emailValue, passwordValue)) {
       this.checkFields(emailValue, passwordValue);
@@ -133,5 +152,32 @@ export class LoginComponent implements OnInit {
       this.login();
       this.loginFormular.reset();
     }
+  }
+
+  setRememberMe(value: boolean) {
+    localStorage.setItem(this.rememberMeKey, JSON.stringify(value));
+    if (value) {
+      localStorage.setItem('email', JSON.stringify(this.loginFormular.controls['emailField'].value));
+    } else {
+      localStorage.setItem('email', '');
+    }
+  }
+
+  getRememberMe(): boolean {
+    const rememberMe = localStorage.getItem(this.rememberMeKey);
+    return rememberMe ? JSON.parse(rememberMe) : false;
+  }
+
+  toggleRememberMe() {
+    this.rememberMe = !this.rememberMe;
+  }
+
+  autoLogin() {
+    this.rememberMe = this.getRememberMe();
+    const email = localStorage.getItem('email');
+    if (this.rememberMe && email) {
+      this.loginFormular.controls['emailField'].setValue(JSON.parse(email));
+    }
+    this.checkboxField.checked = this.rememberMe;
   }
 }
