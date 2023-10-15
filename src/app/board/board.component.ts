cb71421f253fa77ce7151ba8686ec192802c89e0
Gheorghii28 <popovici.gheorghii89@gmail.com>
2023-10-15 19:30:15 +0200

    Implemented created-task

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { UsersListServices } from '../services/firebase-services/users-list.services';
 
 @Component({
   selector: 'app-board',
@@ -7,4 +9,51 @@ import { Component } from '@angular/core';
 })
 export class BoardComponent {
 
+  loading: boolean = true;
+  userId: any;
+  user: any;
+  tasks: any;
+  todoTasks: any;
+  inProgressTasks: any;
+  awaitFeedbackTasks: any;
+  doneTasks: any;
+  
+  constructor(
+    private usersListRef: UsersListServices
+  ) { }
+  
+  ngOnInit(): void {
+    this.initializeUserId();
+    this.updateData().then(() => {
+      this.loading = false;
+    });
+  }
+
+  initializeUserId() {
+    const storedValue = localStorage.getItem('id-key');
+    this.userId = storedValue ? JSON.parse(storedValue) : null;
+  }
+  
+  async updateData() {
+    await this.getUser();
+    this.initializeTasks();
+  }
+
+  async getUser() {
+    const user: User = await this.usersListRef.fetchUserData('users', this.userId);
+    this.user = user;
+  }
+  
+  initializeTasks() {
+    this.tasks = this.user[`tasks`];
+    this.filterTasks();
+    console.log('tasks:', this.tasks);
+  }
+
+  filterTasks() {
+    this.todoTasks = this.tasks.filter((task:any) => task.status === 'to-do');
+    this.inProgressTasks = this.tasks.filter((task:any) => task.status === 'in-progress');
+    this.awaitFeedbackTasks = this.tasks.filter((task:any) => task.status === 'await-feedback');
+    this.doneTasks = this.tasks.filter((task:any) => task.status === 'done');
+  }
 }
