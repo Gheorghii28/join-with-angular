eab08469fd3b9b31bcd010ad80d74eed94381b8f
Gheorghii28 <popovici.gheorghii89@gmail.com>
2023-11-05 11:52:55 +0100

    added user initials in header component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { UsersListServices } from '../services/firebase-services/users-list.services';
+import { onSnapshot } from '@firebase/firestore';
+import { DataServices } from '../services/data-services/data.services';
 
 @Component({
   selector: 'app-header',
@@ -7,10 +10,30 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
+  user:any;
+  userId: any;
+  userInitials:any;
+  unsubUser;
   submenuShow:boolean = false;
 
-  constructor() {
+  constructor(
+    private userListService: UsersListServices,
+    private dataService: DataServices
+  ) {
+    const storedValue = localStorage.getItem('id-key');
+    this.userId = storedValue ? JSON.parse(storedValue) : null;
+    this.unsubUser = this.subUserList();
+  }
 
+  ngOnDestroy() {
+    this.unsubUser();
+  }
+
+  subUserList() {
+    return onSnapshot(this.userListService.getUserDocRef('users', this.userId), (list: any) => {
+      this.user = list.data();
+      this.userInitials = this.dataService.getInitials(this.user.name);
+    })
   }
 
   reloadPage() {
